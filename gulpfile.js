7790253f33370f531da94741288f8fe710208b6d
530274949@qq.com <530274949@qq.com>
2022-02-07 23:50:53 +0800

    feat: 更新

--- gulpfile.js
+++
@@ -1,4 +0,0 @@
-let gulp = require("gulp");
-gulp.task("default", function () {
-  console.log("运行gulp");
-});
---
+++ gulpfile.js/index.js
@@ -0,0 +1,5 @@
+const { build } = require("./gulpfile-build.js");
+const { dev } = require("./gulpfile-dev.js");
+
+exports.build = build;
+exports.dev = dev;
